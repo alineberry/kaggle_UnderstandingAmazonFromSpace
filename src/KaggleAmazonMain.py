70695e21e65b83f10ad5b6bba609f8a30a20b72f
Claire Longo <clongo@mosaicint.local>
2017-05-14 11:58:00 -0400

    refactored KaggleAmazonMain.py

--- src/KaggleAmazonMain.py
+++ src/KaggleAmazonMain.py
@@ -1,10 +1,3 @@
-
-# coding: utf-8
-
-# # Exploratory Data Analysis
-
-# In[15]:
-
 import pandas as pd
 import numpy as np
 import os
@@ -17,52 +10,24 @@ import glob
 import math
 
 
-# In[16]:
-
-#get and print current directory
-cwd = os.getcwd()
-cwd
-
-
-# In[17]:
-
-#open data from current directory. Should work with any direcotry path
-with open(os.path.join(cwd, "data", "train.csv")) as file:
-    tagged_df = pd.read_csv(file)
-tagged_df.head()
-
-
-# In[18]:
-
-#split the tags into new rows
-tagged_df = pd.DataFrame(tagged_df.tags.str.split(' ').tolist(), index=tagged_df.image_name).stack()
-tagged_df = tagged_df.reset_index()[[0, 'image_name']] # dataframe with two columns
-tagged_df.columns = ['tags', 'image_name'] # rename columns
-tagged_df.set_index('image_name', inplace=True) # rest index to image_name again
-
-#create dummy variables for each tag
-tagged_df = pd.get_dummies(tagged_df['tags']) # creates dummy rows
-tagged_df = tagged_df.groupby(tagged_df.index).sum() # adds dummy rows together by image_name index
-tagged_df.head()
-
-
-# In[19]:
-
-#Barplot of tag counts
-get_ipython().magic('matplotlib inline')
-plt.rcParams['figure.figsize'] = (12, 5)
-print('There are {} unique tags in this data'.format(len(tagged_df.columns)))
-colors = cm.rainbow(np.linspace(0, 1, len(tagged_df.columns)))
-tagged_df.sum().sort_values(ascending=False).plot(title="Counts of Tags", color=colors, kind='bar')
-plt.show()
-tagged_df.sum().sort_values(ascending=False)
-
-
-# # Load Image Data
-
-# In[20]:
-
 def load_training_data(ftype='jpg'):
+
+	cwd = os.getcwd()
+
+	#open data from current directory. Should work with any direcotry path
+	with open(os.path.join(cwd, "data", "train.csv")) as file:
+		tagged_df = pd.read_csv(file)
+
+	#split the tags into new rows
+	tagged_df = pd.DataFrame(tagged_df.tags.str.split(' ').tolist(), index=tagged_df.image_name).stack()
+	tagged_df = tagged_df.reset_index()[[0, 'image_name']] # dataframe with two columns
+	tagged_df.columns = ['tags', 'image_name'] # rename columns
+	tagged_df.set_index('image_name', inplace=True) # rest index to image_name again
+
+	#create dummy variables for each tag
+	tagged_df = pd.get_dummies(tagged_df['tags']) # creates dummy rows
+	tagged_df = tagged_df.groupby(tagged_df.index).sum() # adds dummy rows together by image_name index
+
     train_imgs = []
     labels = []
     im_names = []
@@ -79,49 +44,17 @@ def load_training_data(ftype='jpg'):
         
         labels_temp = tagged_df.loc[imname]
         labels.append(labels_temp)
-        
-        
-        
     train_imgs = np.asarray(train_imgs)
     return train_imgs, labels, im_names
 
-
-# In[21]:
-
-X_train, y_train, names_train = load_training_data()
-
-
-# In[22]:
-
-# 100 files, images are 256x256 pixels, with a channel dimension size 3 = RGB
-print('X_train is a {} object'.format(type(X_train)))
-print('it has shape {}'.format(X_train.shape))
-
-
-# In[23]:
-
-print('y_train is a {} object'.format(type(y_train)))
-print('it has {} elements'.format(len(y_train)))
-print('each element is of type {}'.format(type(y_train[0])))
-print('and the elements are of size {}'.format(y_train[0].shape))
-
-
-# In[24]:
-
-print('names_train is a {} object'.format(type(names_train)))
-print('it has {} elements'.format(len(names_train)))
-print('each element is of type {}'.format(type(names_train)))
-
-
-# In[25]:
-
-def get_labels(fname):
+	
+def get_labels(fname, tagged_df):
+	"""return list of labels for a given filename"""
     return ", ".join(tagged_df.loc[fname][tagged_df.loc[fname]==1].index.tolist())    
 
 
-# In[26]:
-
-def plot_samples(nrow, ncol):
+def plot_samples(X_train, nrow, ncol):
+	"""Plots random sample images with their titles and tag names"""
     sampling = np.random.randint(low=0, high=X_train.shape[0]-1, size = nrow*ncol)
     fig, axes = plt.subplots(nrow, ncol, figsize=(15, 12))
     for i in range(0,len(sampling)):
@@ -142,65 +75,18 @@ def plot_samples(nrow, ncol):
     plt.tight_layout()
 
 
-# In[27]:
-
-plot_samples(4,4)
-
-
-# In[28]:
-
-X_train[0,:,:,2]
-
-
-# In[29]:
-
-'train_10'
-
-
-# # Feature Engineering
-# What type of features are we working with here?
-
-# In[30]:
-
-print("There are {} unique features per image".format((256*256)*3))
-
-
-# In[31]:
-
-red = X_train[:,:,:,0].reshape(100, 256*256) # row of red pixel features for all images
-green = X_train[:,:,:,1].reshape(100, 256*256) # row of blue pixel features for all images
-blue = X_train[:,:,:,2].reshape(100, 256*256) #row of green pixel features for all images
-
-
-# In[32]:
-
-red = pd.DataFrame(red, index=???)
-
-
-# In[113]:
-
-fig, axes = plt.subplots(1, 3, figsize=(15, 12))
-axes[0].imshow(X_train[0,:,:,0], cmap='gray')
-axes[1].imshow(X_train[0,:,:,1])
-axes[2].imshow(X_train[0,:,:,2])
-plt.axis('off')
-
-
-# In[ ]:
-
-fig=plt.figure()
-data=np.arange(900).reshape((30,30))
-for i in range(1,5):
-    ax=fig.add_subplot(2,2,i)        
-    ax.imshow(data)
-
-plt.suptitle('Main title')
-plt.show() 
-
-
-# In[98]:
+def get_rgb_vectors(X_train):
+	"""Returns RBG vectors for each image in the training set (return as tuple)"""
+	print("There are {} unique features per image".format((X_train.shape[1]*X_train.shape[2])*X_train.shape[3]))
+	red = X_train[:,:,:,0].reshape(X_train.shape[0], X_train.shape[1]*X_train.shape[2]) # row of red pixel features for all images
+	green = X_train[:,:,:,1].reshape(X_train.shape[0], X_train.shape[1]*X_train.shape[2]) # row of blue pixel features for all images
+	blue = X_train[:,:,:,2].reshape(X_train.shape[0], X_train.shape[1]*X_train.shape[2]) #row of green pixel features for all images
+	
+	return (red, green, blue)
+	
 
 def plot_rgb_dist(img, title):
+	"""Plots the RGB kde for a given image. Image title passed in as argument (image labels perhaps)"""
     plt.subplots_adjust(wspace=0, hspace=0)
     fig = plt.figure(figsize=(18, 3))
     ax = fig.add_subplot(1,2,1)
@@ -213,25 +99,4 @@ def plot_rgb_dist(img, title):
     plt.axis('off')
 
 
-# In[99]:
-
-pics = ['train_10039', 'train_10059', 'train_10034']
-
-
-# In[141]:
-
-plt.subplots_adjust(wspace=0, hspace=0)
-for i in range(0,3):
-    sample = np.random.randint(low=0, high=X_train.shape[0]-1, size = 1)
-    ind = names_train[sample[0]]
-    tags = get_labels(ind)
-    plot_rgb_dist(X_train[sample[0],:,:,:],tags)
-
-
-# In[86]:
-
-sample = np.random.randint(low=0, high=X_train.shape[0]-1, size = 1)
-sample[0]
-ind = names_train[sample[0]]
-ind
 
