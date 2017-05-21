fac4aa638b9dee578047ae2ecfb9000da70c5ac1
Claire Longo <clongo@mosaicint.local>
2017-05-21 12:00:11 -0400

    updated bimodal feature function

--- develop/claire-lab-notebook.py
+++ develop/claire-lab-notebook.py
@@ -3,7 +3,7 @@
 
 # # Exploratory Data Analysis
 
-# In[1]:
+# In[2]:
 
 import pandas as pd
 import numpy as np
@@ -18,7 +18,7 @@ import glob
 import math
 
 
-# In[2]:
+# In[3]:
 
 cwd = os.getcwd()
 path = os.path.join(cwd, '..', 'src')
@@ -27,22 +27,22 @@ if not path in sys.path:
 del cwd, path
 
 
-# In[3]:
+# In[4]:
 
 import KaggleAmazonMain as kam
 
 
-# In[4]:
+# In[5]:
 
 X_train, y_train, names_train, tagged_df = kam.load_training_data()
 
 
-# In[5]:
+# In[6]:
 
 tagged_df.head()
 
 
-# In[6]:
+# In[7]:
 
 #Barplot of tag counts
 get_ipython().magic('matplotlib inline')
@@ -54,7 +54,7 @@ plt.show()
 tagged_df.sum().sort_values(ascending=False)
 
 
-# In[10]:
+# In[8]:
 
 # 100 files, images are 256x256 pixels, with a channel dimension size 3 = RGB
 print('X_train is a {} object'.format(type(X_train)))
@@ -70,7 +70,7 @@ print('it has {} elements'.format(len(names_train)))
 print('each element is of type {}'.format(type(names_train)))
 
 
-# In[11]:
+# In[9]:
 
 kam.plot_samples(X_train, names_train, tagged_df, nrow=4, ncol=4)
 
@@ -80,7 +80,7 @@ kam.plot_samples(X_train, names_train, tagged_df, nrow=4, ncol=4)
 # Feature engineering explores the feature data, and does feature creation.
 # Each image consists of pixel values in red, geen, and blue color schemes. The patterns in these pixels will  have useful trends for classifying the objects in the images and the image types. Notice how the statistical distributions of the red, green, and blue, pixels differ for different types of tags.
 
-# In[12]:
+# In[10]:
 
 fig, axes = plt.subplots(1, 3, figsize=(10, 6))
 axes[0].imshow(X_train[1,:,:,0], cmap='Reds')
@@ -88,7 +88,7 @@ axes[1].imshow(X_train[1,:,:,1], cmap='Greens')
 axes[2].imshow(X_train[1,:,:,2], cmap='Blues')
 
 
-# In[13]:
+# In[11]:
 
 plt.subplots_adjust(wspace=0, hspace=0)
 for i in range(0,3):
@@ -108,7 +108,7 @@ for i in range(0,3):
 # Could also create an indicatorr feauture from this with bimodal or not. 
 # 
 
-# In[166]:
+# In[29]:
 
 #Binned mode differences
 
@@ -167,49 +167,19 @@ def binned_mode_features_with_diagnostics(img, steps):
     plt.axvline(mo2, color='yellow', linestyle='dashed', linewidth=2)
     plt.show()
     
-    return mods_diff_r, mods_diff_g, mods_diff_b
+    return mods_diff_r[0].round(2), mods_diff_g[0].round(2), mods_diff_b[0].round(2)
 
 img=X_train[2]
 steps=np.arange(start=0,stop=1, step=.01)
-binned_mode_features(img, steps)
-    
+binned_mode_features_with_diagnostics(img, steps)
+
+
+# In[28]:
+
+
 
 
 # In[ ]:
 
-def binned_mode_features(img, steps):
-    ## red ##
-    #split on mean
-    m=img[:,:,0].flatten().mean()
-    left = img[:,:,0].flatten()[img[:,:,0].flatten()<m]
-    right = img[:,:,0].flatten()[img[:,:,0].flatten()>=m]
-    #find mode in left and right
-    max_ind_left = np.histogram(left, bins=steps, density=False)[0].argsort()[-1:]
-    max_ind_right = np.histogram(right, bins=steps, density=False)[0].argsort()[-1:]
-    #calc bimodal metric
-    mo1 = np.histogram(right, bins=steps, density=False)[1][max_ind_right]
-    mo2 = np.histogram(left, bins=steps, density=False)[1][max_ind_left]
-    mods_diff_r=abs(mo1-mo2)
 
-    ## green ##
-    m=img[:,:,1].flatten().mean()
-    left = img[:,:,1].flatten()[img[:,:,1].flatten()<m]
-    right = img[:,:,1].flatten()[img[:,:,1].flatten()>=m]
-    max_ind_left = np.histogram(left, bins=steps, density=False)[0].argsort()[-1:]
-    max_ind_right = np.histogram(right, bins=steps, density=False)[0].argsort()[-1:]
-    mo1 = np.histogram(right, bins=steps, density=False)[1][max_ind_right]
-    mo2 = np.histogram(left, bins=steps, density=False)[1][max_ind_left]
-    mods_diff_g=abs(mo1-mo2)
-
-    ## blue ##
-    m=img[:,:,2].flatten().mean()
-    left = img[:,:,2].flatten()[img[:,:,2].flatten()<m]
-    right = img[:,:,2].flatten()[img[:,:,2].flatten()>=m]
-    max_ind_left = np.histogram(left, bins=steps, density=False)[0].argsort()[-1:]
-    max_ind_right = np.histogram(right, bins=steps, density=False)[0].argsort()[-1:]
-    mo1 = np.histogram(right, bins=steps, density=False)[1][max_ind_right]
-    mo2 = np.histogram(left, bins=steps, density=False)[1][max_ind_left]
-    mods_diff_b=abs(mo1-mo2)
-
-    return mods_diff_r, mods_diff_g, mods_diff_b
 
